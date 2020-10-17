62d39bfdeb0ffbada9ae01163844d257ec9935f2
Marten <marten.dr.degroot@gmail.com>
2020-10-16 20:13:21 -0500

    Removed unnecessary code

--- src/BotWrapper.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ScriptTag from 'react-script-tag';
- 
-export class Demo extends Component {
- 
-    render() {
-        return (<ScriptTag isHydrating={true} type="text/javascript" src="Bot.js" />);
-    }
-}
